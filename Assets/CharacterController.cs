5512eae872df4cb5bf23996ebf4bf4fdd17e4c7c
Rodney <rcalacon@gmail.com>
2021-03-10 22:07:47 -0800

    basic movement is there

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float jumpForce = .01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back);
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+}
---
+++ Assets/CharacterView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterView : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 3.5f;
+    private float X;
+    private float Y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * speed, Input.GetAxis("Mouse X") * speed, 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0,.1f,-.3f);
+        }
+    }
+}
