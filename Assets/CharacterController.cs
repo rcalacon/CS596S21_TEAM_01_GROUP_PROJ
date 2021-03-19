71da8561b4253583ba61fd47a5164e79affa6c58
Rodney <rcalacon@gmail.com>
2021-03-18 20:14:33 -0700

    disabled movement in mid air

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float speedLimit = 1f;
-    public float jumpForce = .01f;
+    public float speedLimit = 10f;
+    public float jumpForce = .001f;
     bool isGrounded;
     float ballSpeed = 10.0f;
     float slowDownSpeed = .9f;
@@ -17,33 +17,36 @@ public class CharacterController : MonoBehaviour
 
     void Update()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+        // Disable Movement In Mid Air
+        if (!isGrounded) return;
+
+        Rigidbody player = GetComponent<Rigidbody>();
 
         // General Movement
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(Camera.main.transform.forward * ballSpeed);
+            player.AddForce(Camera.main.transform.forward * ballSpeed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(-Camera.main.transform.right * ballSpeed);
+            player.AddForce(-Camera.main.transform.right * ballSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(Camera.main.transform.right * ballSpeed);
+            player.AddForce(Camera.main.transform.right * ballSpeed);
         }
 
         // Slow Down
         if (Input.GetKey(KeyCode.S))
         {
-            rb.velocity = new Vector3(rb.velocity.x * slowDownSpeed, rb.velocity.y, rb.velocity.z * slowDownSpeed);
+            player.velocity = new Vector3(player.velocity.x * slowDownSpeed, player.velocity.y, player.velocity.z * slowDownSpeed);
         }
         // Jump
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            player.velocity = new Vector3(player.velocity.x, jumpForce, player.velocity.z);
 
-        throttleVelocity(rb);
-        print(rb.velocity);
+        throttleVelocity(player);
+        print(player.velocity);
     }
 
     void throttleVelocity(Rigidbody player)
