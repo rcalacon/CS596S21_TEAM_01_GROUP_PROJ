9ed83d7ffae9d9ec6a4560acbd0a3705e3d97938
Rodney <rcalacon@gmail.com>
2021-04-18 22:00:03 -0700

    timer and score is somewhat working

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boid : MonoBehaviour
+{
+    public Vector3 Position;
+    public Vector3 Velocity;
+    public List<Transform> Neighbors;
+    //public Text gameScore;
+    public GameObject gameManager;
+
+    public Boid(Vector3 position, Vector3 velocity)
+    {
+        Position = position;
+        Velocity = velocity;
+        gameObject.transform.position = position;
+        gameObject.transform.rotation = Quaternion.LookRotation(Vector3.Normalize(velocity));
+    }
+
+    public void ApplyFlockUpdates(Vector3 position, Vector3 velocity)
+    {
+        Position = position;
+        Velocity = velocity;
+        gameObject.transform.position = position;
+        gameObject.transform.rotation = Quaternion.LookRotation(Vector3.Normalize(velocity));
+    }
+
+    public void UpdateNeighbors(List<Transform> boids, float distance)
+    {
+        var neighbors = new List<Transform>();
+
+        for (var neighborIndex = 0; neighborIndex < boids.Count; neighborIndex++)
+        {
+            var neighbor = boids[neighborIndex];
+            if (neighbor == null) continue;
+
+            if (Position != boids[neighborIndex].position)
+            {
+                if (Vector3.Distance(boids[neighborIndex].position, Position) < distance)
+                {
+                    neighbors.Add(boids[neighborIndex]);
+                }
+            }
+        }
+        Neighbors = neighbors;
+    }
+
+    public Vector3 Cohesion(float cohesionSteps, float weight)
+    {
+        var perceivedCenter = Vector3.zero;
+
+        if (Neighbors.Count == 0 || cohesionSteps < 1) return perceivedCenter;
+
+        for (var neighborIndex = 0; neighborIndex < Neighbors.Count; neighborIndex++)
+        {
+            var neighbor = Neighbors[neighborIndex];
+            if (neighbor == null) continue;
+
+            var neighborComponent = neighbor.GetComponent<Boid>();
+            if (perceivedCenter == Vector3.zero)
+            {
+                perceivedCenter = neighborComponent.Position;
+            }
+            else
+            {
+                perceivedCenter = perceivedCenter + neighborComponent.Position;
+            }
+        }
+        perceivedCenter = perceivedCenter / Neighbors.Count;
+        return (perceivedCenter - Position) / cohesionSteps * weight;
+    }
+
+    public Vector3 Separation(float weight)
+    {
+        var c = Vector3.zero;
+
+        for (var i = 0; i < Neighbors.Count; ++i)
+        {
+            var neighbor = Neighbors[i];
+            if (neighbor == null) continue;
+
+            var neighborComponent = neighbor.GetComponent<Boid>();
+            var distance = Vector3.Distance(Position, neighborComponent.Position);
+
+            c = c + Vector3.Normalize(Position - neighborComponent.Position) / Mathf.Pow(distance, 2);
+        }
+        return c * weight;
+    }
+
+    public Vector3 Alignment(float weight)
+    {
+        Vector3 pv = Vector3.zero;
+
+        if (Neighbors.Count == 0) return pv;
+
+        for (var neighborIndex = 0; neighborIndex < Neighbors.Count; neighborIndex++)
+        {
+            var neighbor = Neighbors[neighborIndex];
+            if (neighbor == null) continue;
+
+            var neighborComponent = neighbor.GetComponent<Boid>();
+            pv = pv + neighborComponent.Velocity;
+        }
+        if (Neighbors.Count > 1)
+        {
+            pv = pv / (Neighbors.Count);
+        }
+        return (pv - Velocity) * weight;
+    }
+
+    public Vector3 Seek(Transform target)
+    {
+        var desiredVelocity = (target.position - Position);
+        return desiredVelocity - Velocity;
+    }
+
+    public Vector3 Socialize(List<Transform> boids)
+    {
+         var pc = Vector3.zero; 
+
+        if (Neighbors.Count != 0) return pc;
+
+        for (var i = 0; i < boids.Count; ++i)
+        {
+            var boid = boids[i];
+            if (boid == null) continue;
+
+            var boidComponent = boid.GetComponent<Boid>();
+            if (Position != boidComponent.Position)
+            {
+                if (pc == Vector3.zero)
+                {
+                    pc = boidComponent.Position;
+                }
+                else
+                {
+                    pc = pc + boidComponent.Position;
+                }
+            }
+        }
+        if (boids.Count > 1)
+        {
+            pc = pc / (boids.Count - 1);
+        }
+        return Vector3.Normalize(pc - Position);
+    }
+
+    public Vector3 Arrival(Transform target, float slowingDistance, float maxSpeed)
+    {
+        var desiredVelocity = Vector3.zero;
+        if (slowingDistance < 0.0001f) return desiredVelocity;
+
+        var targetOffset = target.position - Position;
+        var distance = Vector3.Distance(target.position, Position);
+        var rampedSpeed = maxSpeed * (distance / slowingDistance);
+        var clippedSpeed = Mathf.Min(rampedSpeed, maxSpeed);
+        if (distance > 0)
+        {
+            desiredVelocity = (clippedSpeed / distance) * targetOffset;
+        }
+        return desiredVelocity - Velocity;
+    }
+
+    public Vector3 LimitVelocity(Vector3 v, float limit)
+    {
+        if (v.magnitude > limit)
+        {
+            v = v / v.magnitude * limit;
+        }
+        return v;
+    }
+
+    public Vector3 LimitRotation(Vector3 v, float maxAngle, float maxSpeed)
+    {
+        return Vector3.RotateTowards(Velocity, v, maxAngle * Mathf.Deg2Rad, maxSpeed);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Hit by Cannonball
+        if (col.gameObject.tag == "Cannonball")
+        {
+            gameManager.GetComponent<CannonBoidsManager>().updateScore();
+            //updateScore();
+            Destroy(this.gameObject);
+        }
+    }
+
+    /*void updateScore()
+    {
+        char space = ' ';
+        int score = int.Parse(gameScore.text.Split(space)[1]);
+        score++;
+        print("SCORE: " + score.ToString());
+        gameScore.text = "SCORE: " + score.ToString();
+    }*/
+}
---
+++ Assets/Scripts/CannonBoidsManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CannonBoidsManager : MonoBehaviour
+{
+    private string scorePrefix = "SCORE: ";
+    private string timeSuffix = "s LEFT";
+    private int numSeconds = 30;
+    private Text gameScore;
+    private Text timer;
+    private float time;
+
+    public void updateScore()
+    {
+        char space = ' ';
+        int score = int.Parse(gameScore.text.Split(space)[1]);
+        score++;
+        gameScore.text = "SCORE: " + score.ToString();
+    }
+
+    public void updateTimer()
+    {
+        time += Time.deltaTime;
+        int secondsElapsed = Mathf.FloorToInt(time % 60f);
+        int timeLeft = numSeconds - secondsElapsed;
+        if(timeLeft > 0)
+        {
+            timer.text = timeLeft.ToString() + timeSuffix;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameScore = GameObject.Find("Text_Score").GetComponent<Text>();
+        timer = GameObject.Find("Text_Timer").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateTimer();
+    }
+}
